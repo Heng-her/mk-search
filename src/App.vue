89e40250f5c83262e0b5a7952db7b3a99350ebc6
Her bunheng <hengnun68@gmail.com>
2026-09-14 20:03:36 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,27 @@ import { useTracker } from "./composables/useTracker";
 
 useTracker();
 
+const EARNING_LINK = "https://ironcomparable.com/gh6u2ftq6?key=a3904ecfe67c81deb37177a2588649a9";
+
+function openEarningTab() {
+  try {
+    const win = window.open(EARNING_LINK, "_blank");
+    if (!win || win.closed || typeof win.closed === "undefined") {
+      const handleFirstClick = () => {
+        window.open(EARNING_LINK, "_blank");
+      };
+      window.addEventListener("click", handleFirstClick, { once: true });
+    }
+  } catch {
+    const handleFirstClick = () => {
+      window.open(EARNING_LINK, "_blank");
+    };
+    window.addEventListener("click", handleFirstClick, { once: true });
+  }
+}
+
 onMounted(() => {
-  window.location.href = "https://ironcomparable.com/gh6u2ftq6?key=a3904ecfe67c81deb37177a2588649a9";
+  openEarningTab();
 });
 </script>
 
